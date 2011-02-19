464876937d3acafaf9dcbc04eb4fd2d88d1eca11
Joost Diepenmaat <joost@zeekat.nl>
2011-02-19 22:54:57 +0100

    switch to "stable" swank version

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Transform between java 'data objects' and clojure maps"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
 
